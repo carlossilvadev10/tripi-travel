73ff46e4a57ac94df38e278506103aa2806cee33
carlozzsilva10 <0332202039@unjfsc.edu.pe>
2025-06-10 23:52:24 -0500

    Se corrige el error en WhyChooseCard

--- components/Home/WhyChooseUs/WhyChooseCard.tsx
+++ components/Home/WhyChooseUs/WhyChooseCard.tsx
@@ -2,26 +2,25 @@ import React from "react";
 import Image from "next/image";
 
 type Props = {
-    image: string;
-    title: string;
-    description: string;
-    [key: string]: any; // Para aceptar data-aos y similares
-}
+  image: string;
+  title: string;
+  description: string;
+} & React.HTMLAttributes<HTMLDivElement>;
 
 const WhyChooseCard = ({ image, title, description, ...rest }: Props) => {
     return (
         <div {...rest}>
-            {/* Image */}
-            <Image src = {image} alt = "image" width = {70} height = {70} className = "mx-auto" />
-            {/* Content */}
-            <h1 className = "mt-6 text-center text-gray-900 font-medium text-lg">
-                {title}
-            </h1>
-            <p className = "mt-2 text-center text-xs font-medium text-gray-700">
-                {description}
-            </p>
+        {/* Imagen */}
+        <Image src={image} alt="image" width={70} height={70} className="mx-auto" />
+        {/* Contenido */}
+        <h1 className="mt-6 text-center text-gray-900 font-medium text-lg">
+            {title}
+        </h1>
+        <p className="mt-2 text-center text-xs font-medium text-gray-700">
+            {description}
+        </p>
         </div>
-    )
-}
+  );
+};
 
 export default WhyChooseCard;
