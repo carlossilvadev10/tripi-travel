9e76ef3cc15efe184ce0b1715e98842c95dfd1fe
carlozzsilva10 <0332202039@unjfsc.edu.pe>
2025-06-14 12:26:21 -0500

    Se añade la propiedad cursor:pointer a ScrollToTop.jsx

--- components/Helper/ScrollToTop.tsx
+++ components/Helper/ScrollToTop.tsx
@@ -29,7 +29,7 @@ const ScrollToTop = () => {
         <div className = "fixed bottom-4 animate-pulse right-4">
             {
                 isVisible && (
-                    <button className = "bg-rose-700 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none" onClick = {scrollToTop}>
+                    <button className = "bg-rose-700 text-white rounded-full w-12 h-12 flex items-center justify-center focus:outline-none cursor-pointer" onClick = {scrollToTop}>
                         <FaArrowUp />
                     </button>
                 )
